b9454bb2b652ae75346b65053f1caed43619dcb6
Arkar <arkarmin.dev@gmail.com>
2024-09-17 18:25:08 +0700

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Arkar Min on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
